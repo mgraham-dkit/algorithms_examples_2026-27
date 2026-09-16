012bfe6f97eba32218087d107e5fe6b4bbd6e803
michelle <michelle.graham@dkit.ie>
2026-09-16 17:01:58 +0100

    docs: Add javadoc to getValidInteger implementation

--- src/main/java/solutions/week01/CalcMax.java
+++ src/main/java/solutions/week01/CalcMax.java
@@ -3,6 +3,13 @@ package solutions.week01;
 import java.util.Scanner;
 
 public class CalcMax {
+    /**
+     * Safely take in an integer from the user.
+     * This will repeatedly prompt the user using the supplied prompt,
+     * and will detect and recover where non-integer values are entered
+     * @param prompt The text to be displayed to the user
+     * @return The first valid (numeric) integer value entered by the user
+     */
     public static int getValidInteger(String prompt){
         Scanner input = new Scanner(System.in);
         int value = 0;
