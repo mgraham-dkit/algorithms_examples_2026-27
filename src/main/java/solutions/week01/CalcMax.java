ef9471878214b7cf83dd9e795870f114ff7d17e5
michelle <michelle.graham@dkit.ie>
2026-09-16 22:12:07 +0100

    solutions: Sample solution for overloaded getValidInteger() method (including upper and lower bounds)

--- src/main/java/solutions/week01/CalcMax.java
+++ src/main/java/solutions/week01/CalcMax.java
@@ -29,6 +29,39 @@ public class CalcMax {
         return value;
     }
 
+    /**
+     * Safely take in a valid integer from the user between the specified upper and lower limits (inclusive).
+     * This will repeatedly prompt the user using the supplied prompt,
+     * and will detect and recover where non-integer values are entered.
+     * Only values between the specified upper and lower limits will be accepted.
+     * @param prompt The text to be displayed to the user
+     * @param min The lowest value that can be accepted
+     * @param max The highest value that can be accepted
+     * @return The first valid integer entered by the user
+     */
+    public static int getValidInteger(String prompt, int min, int max){
+        Scanner input = new Scanner(System.in);
+        int value = 0;
+
+        boolean validInt = false;
+        while(!validInt) {
+            System.out.println(prompt);
+            if(input.hasNextInt()){
+                value = input.nextInt();
+                if(value >= min && value <= max) {
+                    validInt = true;
+                }else{
+                    System.out.println("Value must be between " + min + " and " + max);
+                }
+            }else{
+                System.out.println("Please enter an integer.");
+                input.nextLine();
+            }
+        }
+
+        return value;
+    }
+
     public static int findMax(int num1, int num2, int num3){
         int max = num1;
 
@@ -49,9 +82,9 @@ public class CalcMax {
     static void main() {
         Scanner input = new Scanner(System.in);
 
-        int num1 = getValidInteger("Please enter the first number: ");
-        int num2 = getValidInteger("Please enter the second number: ");
-        int num3 = getValidInteger("Please enter the final number: ");
+        int num1 = getValidInteger("Please enter the first number: ", 15, 45);
+        int num2 = getValidInteger("Please enter the second number: ", 15, 45);
+        int num3 = getValidInteger("Please enter the final number: ", 10, 10);
 
         int max = findMax(num1, num2, num3);
 
