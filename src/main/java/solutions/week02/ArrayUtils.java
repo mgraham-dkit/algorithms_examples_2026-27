a989f998f83cfa52f7758a91c8daed01a16a9a0d
michelle <michelle.graham@dkit.ie>
2026-09-23 11:01:31 +0100

    docs: add Javadoc for displayArray(int array)

--- src/main/java/solutions/week02/ArrayUtils.java
+++ src/main/java/solutions/week02/ArrayUtils.java
@@ -1,6 +1,11 @@
 package solutions.week02;
 
 public class ArrayUtils {
+    /**
+     * Displays the contents of a supplied int array on the console.
+     * Data is displayed vertically with index included.
+     * @param nums the array to be displayed
+     */
     public static void displayArray(int [] nums){
         for (int i = 0; i < nums.length; i++) {
             System.out.println(i + ": " + nums[i]);
