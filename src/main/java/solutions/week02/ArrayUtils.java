eb672193ad4813d239ec2976ed1bb7d3c42c651f
michelle <michelle.graham@dkit.ie>
2026-09-23 11:01:05 +0100

    feat: add method to display array of ints

---
+++ src/main/java/solutions/week02/ArrayUtils.java
@@ -0,0 +1,9 @@
+package solutions.week02;
+
+public class ArrayUtils {
+    public static void displayArray(int [] nums){
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(i + ": " + nums[i]);
+        }
+    }
+}
