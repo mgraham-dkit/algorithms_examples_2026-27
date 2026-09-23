29a4552090485b22bcf12bbcdd9669338b664b33
michelle <michelle.graham@dkit.ie>
2026-09-23 11:03:08 +0100

    feat: add displayArray method for Strings

--- src/main/java/solutions/week02/ArrayUtils.java
+++ src/main/java/solutions/week02/ArrayUtils.java
@@ -11,4 +11,15 @@ public class ArrayUtils {
             System.out.println(i + ": " + nums[i]);
         }
     }
+
+    /**
+     * Displays the contents of a supplied String array on the console.
+     * Data is displayed vertically with index included.
+     * @param nums the array to be displayed
+     */
+    public static void displayArray(String [] nums){
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(i + ": " + nums[i]);
+        }
+    }
 }
